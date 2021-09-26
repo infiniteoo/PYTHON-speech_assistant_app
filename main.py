683c6810a5a442a7481edfaad1bf82a6c6cce18a
Troy Dorman <troydorman@gmail.com>
2021-09-26 14:23:30 -0600

    created respond function to answer the inputted voice data

--- main.py
+++ main.py
@@ -2,14 +2,24 @@ import speech_recognition as sr
 
 r = sr.Recognizer()
 
-with sr.Microphone() as source:
-    print('Say something')
-    audio = r.listen(source)
-    try:
-      voice_data = r.recognize_google(audio)
-      print(voice_data)
-    except sr.UnknownValueError:
-        print('Sorry, I did not get that')
-    except sr.RequestError:
-        print('Sorry, my speech service is down') 
+def record_audio():
+    with sr.Microphone() as source:
+        
+        audio = r.listen(source)
+        voice_data = ''
+        try:
+            voice_data = r.recognize_google(audio)
+        except sr.UnknownValueError:
+            print('Sorry, I did not get that')
+        except sr.RequestError:
+            print('Sorry, my speech service is down') 
+        return voice_data
+
+def respond(voice_data):
+    if 'what is your name' in voice_data:
+        print('my name is alexis')
+
+print('How can I help you?')
+voice_data = record_audio()
+respond(voice_data)
 
